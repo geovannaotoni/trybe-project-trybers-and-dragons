d2c5b445ec351b73d217b0bc4576ce0189074fdc
Geovanna Otoni <geovannaotoni@gmail.com>
2023-09-19 16:03:15 -0300

    feat01: abstract class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,20 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name() { return this._name; }
+  public get dexterity() { return this._dexterity; }
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
